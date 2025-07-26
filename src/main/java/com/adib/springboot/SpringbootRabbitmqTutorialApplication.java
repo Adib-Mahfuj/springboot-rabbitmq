5155a06660eac00369f5562fc59e71bc4ba47e94
Adib-Mahfuj <adibmahfuj420@gmail.com>
2025-07-26 10:09:05 +0600

    Initial commit

---
+++ src/main/java/com/adib/springboot/SpringbootRabbitmqTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.adib.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqTutorialApplication.class, args);
+	}
+
+}
