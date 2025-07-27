b76bbcb2bb2758ec1f78517cc4c845518b691686
Adib-Mahfuj <adibmahfuj420@gmail.com>
2025-07-27 17:04:43 +0600

    feat(rabbitmq): create producer to send JSON messages

---
+++ src/main/java/com/adib/springboot/publisher/RabbitMQJSONProducer.java
@@ -0,0 +1,37 @@
+package com.adib.springboot.publisher;
+
+import com.adib.springboot.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQJSONProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJSONProducer.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitMQJSONProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendJsonMessage(User user) {
+        LOGGER.info(String.format("JSON message sent -> %s", user.toString()));
+        rabbitTemplate.convertAndSend(exchange, routingJsonKey, user);
+    }
+
+
+
+
+
+
+}
